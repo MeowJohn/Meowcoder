3c9110760abdc15deb0077401dd6ee6113c790eb
notauid <benjamin.john@thundertech.com>
2014-07-03 16:17:32 -0400

    Update meowcoder-app.js

--- meowcoder-app.js
+++ meowcoder-app.js
@@ -1,5 +1,10 @@
 meow.meowcoder.App = function ( ) {
-
+  
+  var _encoder, _decoder;
+  
+  _encoder = new meow.meowcoder.Encoder();
+  _decoder = new meow.meowcoder.Decoder();
+  
 };
 
 meow.meowcoder.app = new meow.meowcoder.App();
