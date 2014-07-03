def8147462009cbf58cdb4d4f91db8d687e4cde6
notauid <benjamin.john@thundertech.com>
2014-07-03 16:23:01 -0400

    Update meowcoder-app.js

--- meowcoder-app.js
+++ meowcoder-app.js
@@ -1,9 +1,11 @@
 meow.meowcoder.App = function ( ) {
   
-  var _encoder, _decoder;
+  var _encoder, _decoder, _textToTranslateField, _resultField;
   
   _encoder = new meow.meowcoder.Encoder();
   _decoder = new meow.meowcoder.Decoder();
+  _textToTranslateField = document.getElementById ( 'text-to-translate-field' );
+  _resultField = document.getElementById ( 'result-field' );
   
 };
 
