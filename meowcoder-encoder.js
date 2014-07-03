9734d0854e7b0686de85625e46e2ef45c0b46c08
notauid <benjamin.john@thundertech.com>
2014-07-03 17:06:54 -0400

    Update meowcoder-encoder.js

--- meowcoder-encoder.js
+++ meowcoder-encoder.js
@@ -5,7 +5,14 @@ meow.meowcoder.Encoder = function ( ) {
   
   _this.setText = function ( _newText ) {
     _text = _newText;
+  };
+
+  _this.getTranslation = function ( ) {
+    var _result;
     _currentCharacterIndex = 0;
+    while ( _areThereMoreResults ( ) )
+      _result += _getNextResult ( );
+    return _result;
   };
 
 };
