2a4c09dc6efedb3481a9f00187c7ce0d4bb6d65f
notauid <benjamin.john@thundertech.com>
2014-07-05 23:45:31 -0400

    Update meowcoder-lang.js

--- meowcoder-lang.js
+++ meowcoder-lang.js
@@ -3,7 +3,7 @@ meow.meowcoder.Lang = function () {
   var _dictionary, _this;
   
   _dictionary = [];
-  _this = this
+  _this = this;
   
   _this.assignEnglishToMeow = function (_english, _meow) {
     var _definition;
@@ -11,5 +11,23 @@ meow.meowcoder.Lang = function () {
     _definition.english = _english;
     _definition.meow = _meow;
    _dictionary.push (_definiton);
-  }
+  };
+  
+  _this.getLengthOfMeowFromEnglishAt = function ( _english, _at ) {
+    return 2;
+  };
+  
+  _this.getLengthOfEnglishFromMeowAt = function ( _english, _at ) {
+    return 1;
+  };
+  
+  _this.getEnglishFromMeow = function ( _meow ) {
+    return 'a';
+  };
+
+  _this.getMeowFromEnglish = function ( _english ) {
+    return 'me';
+  };
+
+
 }
